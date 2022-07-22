e7137a4263754d22e3ad6b827c85bd865e88c9f9
Bun-letsgogo <bun.coding@gmail.com>
2022-07-22 08:30:28 +0800

    setup handlebars and static files

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
-
+const exhbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send(`Hi!`)
+  res.render('index')
 })
 
 app.listen(port, () => {
